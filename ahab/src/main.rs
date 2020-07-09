e940e3e6498bc297bd24ad4cb5626e4fad4cc4e7
roger <rogerduran@gmail.com>
2020-07-09 12:26:41 +0200

    panic at the disco

--- ahab/src/main.rs
+++ ahab/src/main.rs
@@ -5,12 +5,20 @@ fn main() {
     let threads = std::env::var("THREADS")
         .unwrap_or_else(|_| "100".into())
         .parse::<usize>().unwrap();
-    println!("Running with {} threads", threads);
+
+    let crash = std::env::var("CRASH")
+        .unwrap_or_else(|_| "0".into())
+        .parse::<usize>().unwrap();
+
+    println!("Running with {} threads crashing in {} loops", threads, crash);
     let mut handlers = vec![];
     for i in 0..threads{
-        let handler = thread::spawn(move || loop {
+        let handler = thread::spawn(move || for x in 0.. {
             println!("Thread {} Running", i);
             thread::sleep(Duration::from_secs(1));
+            if crash > 0 && crash < x {
+                panic!("at the disco {}", x);
+            }
         });
         handlers.push(handler);
     }
